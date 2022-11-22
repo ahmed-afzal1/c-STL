6aad809e205acd358d53c06d2c55e9258486bb0d
ahmed-afzal1 <afzalbhairab4@gmail.com>
2022-11-22 20:45:59 +0600

    auto keyword

---
+++ auto_keyword.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    vector<int> v={1,2,3,4,5};
+
+    for(auto value:v){
+        cout<<value;
+    }
+
+    return 0;
+}
